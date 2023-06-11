1bd7e1252d60c8ff8fcb90084fde6bc7a254b183
andylamax <andylamax@programmer.net>
2023-06-12 00:14:00 +0300

    Ditching Multiplatform FileChooser

--- live/compose/src/commonMain/kotlin/cinematic/WatchAsState.kt
+++ live/compose/src/commonMain/kotlin/cinematic/WatchAsState.kt
@@ -1,3 +1,5 @@
+@file:Suppress("NOTHING_TO_INLINE")
+
 package cinematic
 
 import androidx.compose.runtime.getValue
@@ -8,10 +10,13 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.DisposableEffect
 
 @Composable
-fun <S> Live<S>.watchAsState(): S {
-    var state by remember { mutableStateOf(value) }
-    DisposableEffect(this) {
-        val watcher = watch(WatchMode.Casually) { state = it }
+inline fun <S> Live<S>.watchAsState(): S = rememberLive(this)
+
+@Composable
+fun <S> rememberLive(live: Live<S>): S {
+    var state by remember(live) { mutableStateOf(live.value) }
+    DisposableEffect(live) {
+        val watcher = live.watch(WatchMode.Casually) { state = it }
         onDispose { watcher.stop() }
     }
     return state

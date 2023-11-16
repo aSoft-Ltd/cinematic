5971a9b81e620d6b8e598789d6f4ded52a7465f7
andylamax <andylamax@programmer.net>
2023-11-16 13:15:17 +0300

    Syncing with george

--- scene/core/src/commonMain/kotlin/cinematic/LazyScene.kt
+++ scene/core/src/commonMain/kotlin/cinematic/LazyScene.kt
@@ -1,19 +1,21 @@
 @file:JsExport
 @file:Suppress("NON_EXPORTABLE_TYPE")
+@file:OptIn(ExperimentalJsExport::class)
 
 package cinematic
 
 import kase.LazyState
 import kase.Pending
 import kase.Success
+import kotlin.js.ExperimentalJsExport
 import kotlin.js.JsExport
 import kotlin.js.JsName
 
 abstract class LazyScene<out S>(state: LazyState<S>) : Scene<LazyState<S>>(state) {
-    @JsName("asPending")
+    @JsExport.Ignore
     constructor() : this(Pending)
 
-    @JsName("asSuccess")
+    @JsExport.Ignore
     constructor(state: S) : this(Success(state))
 
     open fun deInitialize() {

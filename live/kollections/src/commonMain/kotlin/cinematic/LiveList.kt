b7e19da8961d9a7c27d70e7b44e222ebde85432b
andylamax <andylamax@programmer.net>
2023-12-24 13:35:57 +0300

    Migrated picortex to K2

--- live/kollections/src/commonMain/kotlin/cinematic/LiveList.kt
+++ live/kollections/src/commonMain/kotlin/cinematic/LiveList.kt
@@ -4,5 +4,7 @@ package cinematic
 
 import kollections.List
 import kotlinx.JsExport
+import kotlinx.JsName
 
+@JsName("LiveArray")
 interface LiveList<out E> : Live<List<E>>
--- live/kollections/src/commonMain/kotlin/cinematic/MutableLiveList.kt
+++ live/kollections/src/commonMain/kotlin/cinematic/MutableLiveList.kt
@@ -6,7 +6,9 @@ package cinematic
 import kollections.List
 import kollections.MutableList
 import kotlinx.JsExport
+import kotlinx.JsName
 
+@JsName("MutableLiveArray")
 interface MutableLiveList<E> : MutableLiveCollection<E>, MutableLive<List<E>>, LiveList<E> {
     fun <R> update(block: (MutableList<E>) -> R): R
 }
